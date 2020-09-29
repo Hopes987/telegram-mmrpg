5d48bb1b0aef839eb13f555dc8446b3ff3c24a5a
egorkaBurkenya <sleepyegorka@gmail.com>
2020-09-29 23:33:51 +0300

    app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def reg_class(message):
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
             item1 = types.KeyboardButton("/menu")
             markup.add(item1)
-            bot.send_message(message.chat.id, 'Ты успешно попал в мир ! друг мой', replay_markup=markup)
+            bot.send_message(message.chat.id, 'Ты успешно попал в мир ! друг мой', reply_markup=markup)
         else:
             bot.send_message(message.chat.id, 'Такого класса не существует !')
 
@@ -50,13 +50,36 @@ def info(message):
 
 @bot.message_handler(commands=['menu'])
 def menu(message):
-    
+    player = World.return_player(message.chat.id)
+    if player._class == 'Маг':
+                if player.weapon.name == 'magick stick':
+                    bot.send_message(message.chat.id, player.name)
+                    markup = types.InlineKeyboardMarkup(row_width=1)
+                    item1 = types.InlineKeyboardButton("Мои характеристики", callback_data='info')
+                    item2 = types.InlineKeyboardButton("Рюкзак", callback_data='backpack')
+                    markup.add(item1, item2)
+                    bot.send_message(message.chat.id, f'{"❤" * player.helf}                 {player._class}')
+                    bot.send_photo(message.chat.id, 'https://ibb.co/WsdTpHN', reply_markup=markup)
 
-@bot.message_handler(commands=['iii'])
-def test(message):
-    for i in World.players:
-        if i.id == int(message.text.split(' ')[1]):
-            bot.send_message(message.chat.id, i.name)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'info':
+                i = World.return_player(call.message.chat.id)
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+                item1 = types.KeyboardButton("/close")
+                markup.add(item1)
+                bot.send_message(call.message.chat.id, f'Имя: {i.name}\nКласс: {i._class}\nЗдоровье: {i.helf}\nСила: {i.power}\nОружие: {i.weapon.name}', reply_markup=markup)
+            elif call.data == 'backpack':
+                bot.send_message(call.message.chat.id, 'noting')
+    except Exception as e:
+        print(repr(e))
+
+@bot.message_handler(commands=['close'])
+def close(message): pass
 
 
 bot.polling(none_stop=True)
