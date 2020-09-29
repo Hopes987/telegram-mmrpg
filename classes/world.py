e80dd3811d559c5e001efa114e8aab0c426646ff
egorkaBurkenya <sleepyegorka@gmail.com>
2020-09-29 14:49:09 +0300

    return_player

--- classes/world.py
+++ classes/world.py
@@ -8,3 +8,7 @@ class World:
     def add_player(player):
         World.players.append(player)
     
+    @staticmethod
+    def return_player(player_id):
+        for i in World.players: if i.id == player_id: return i
+    
